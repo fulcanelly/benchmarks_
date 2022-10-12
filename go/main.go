1bb61792716259d91173bb97f61c2f1609d77d64
fulcanelly <dallasxfulcanelly@gmail.com>
2022-10-12 20:50:20 +0300

    fut: make golang version multithread

--- go/main.go
+++ go/main.go
@@ -3,44 +3,135 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 	"time"
 )
+
+var lib [4]byte = [4]byte{44, 12, 112, 224}
+
+
+func getNumber_(x *rand.Rand) byte {
+	return lib[x.Intn(4)]
+}
+
+
+const size = 1_000_000_000
 
 func getRand() *rand.Rand {
 	source := rand.NewSource(time.Now().UnixNano())
 	return rand.New(source)
 }
 
-func main() {
-	var list []byte
-	const size = 1000 * 1000 * 1000
-	n := 0
 
-	var lib [4]byte
-	lib[0] = 44
-	lib[1] = 12
-	lib[2] = 112
-	lib[3] = 224
+type Pair[T interface {}] struct {
+	start T
+	end   T
+}
 
-	var rand_ = getRand()
-	fmt.Println("Generating list..")
+func GenerateChunk(
+	rands *rand.Rand,
+	input []byte,
+	_range Pair[int],
+) {
+	fmt.Println("Generating chunk ", _range)
+	var i = _range.start 
 
-	for n < size {
-		n += 1
-		list = append(
-			list, lib[rand_.Intn(4)],
-		)
+	for i < _range.end {
+		var num = getNumber_(rands)
+		input[i] = num
+		i++
+	}
+	fmt.Println("Done chunk ", _range)
+
+}
+
+func CountOccursInRage(
+	input []byte,
+	_range Pair[int],
+	results chan map[byte]int,
+) {
+	fmt.Println("Counting chunk ", _range)
+
+	var i = _range.start 
+	var result map[byte]int
+	result = make(map[byte]int)
+
+	for i < _range.end {
+		result[input[i]]++		
+		i++ 
 	}
 
+	fmt.Println("Ending chunk ", _range)
+	results <- result
+}
 
-	var stats map[byte]int;
+func MergeCountMaps(
+	first map[byte]int, 
+	second map[byte]int,
+) map[byte]int {
+	var result map[byte]int = first
+
+	for key, val := range second {
+		result[key] += val
+	}
 	
-	stats = make(map[byte]int);
-	fmt.Println("Counting...")
+	return result
+}
 
-	for _, item := range list {
-		stats[item]++
-	} 
+func GenAndCountChunk(
+	input []byte,
+	_range Pair[int],
+	results chan map[byte]int,
+) {
+	GenerateChunk(getRand(), input, _range)
+	CountOccursInRage(input, _range, results)
+}
+
+
+func GenNCountParallel(
+	input []byte,
+	chunk_size int, 
+	max int,
+) map[byte]int {
+	var start = 0
+
+	var chans []chan map[byte]int
+	
+	for start < max {
+		var recv = make(chan map[byte]int)
+		chans = append(chans, recv);
+
+		var _range =  Pair[int]{
+			start, start + chunk_size,
+		}
+
+		go GenAndCountChunk(input, _range, recv);
+		
+		start += chunk_size
+
+	}
+
+ 	var res map[byte]int 
+
+	for _, ch := range chans {
+		res = MergeCountMaps(<- ch, res)
+	}
+
+	return res
+}
+
+
+func main() {
+	var cpus = runtime.NumCPU()
+
+	var list []byte 
+
+	var chunk_size = size / cpus
+	
+	list = make([]byte, size)
+
+	var stats map[byte]int = GenNCountParallel(list, chunk_size, size)
+
 
 	fmt.Println("Result:")
 
